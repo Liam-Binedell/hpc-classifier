3b5aba55c66f1bde05bbe1a7fa1f9b103465014c
Liam-Binedell <liam.binedell@gmail.com>
2026-05-04 13:16:45 +0200

    rename imgs and lbls to specify training

--- main.c
+++ main.c
@@ -26,36 +26,39 @@ int argmax(double *vec, int size) {
 int main() {
     srand(time(NULL));
 
-    Images imgs = {NULL, 0, 0, 0, 0};
-    Labels lbls = {NULL, 0};
+    Images train_imgs = {NULL, 0, 0, 0, 0};
+    Labels train_lbls = {NULL, 0};
     Network network;
     Trainer trainer;
 
-    load_images("datasets/train-images.idx3-ubyte", &imgs);
-    load_labels("datasets/train-labels.idx1-ubyte", &lbls);
-    printf("Loaded %u images\n", imgs.size);
-    printf("Loaded %u labels\n", lbls.size);
+    load_images("datasets/train-images.idx3-ubyte", &train_imgs);
+    load_labels("datasets/train-labels.idx1-ubyte", &train_lbls);
+    printf("Loaded %u images\n", train_imgs.size);
+    printf("Loaded %u labels\n", train_lbls.size);
 
-    network_init(&network, imgs.img_size, 64, N_OUTPUTS);
+    network_init(&network, train_imgs.img_size, 64, N_OUTPUTS);
     trainer_init(&trainer, &network);
 
     double y[N_OUTPUTS];
 
+    // TRAIN FOR THE SET AMOUNT OF EPOCHS
     for (int epoch = 0; epoch < EPOCHS; epoch++) {
         int correct = 0;
-        for (int i = 0; i < imgs.size; i++) {
-            one_hot(lbls.data[i], y);
-            trainer_train(&trainer, &network, imgs.data[i], y, LR);
-            if (argmax(network.output, N_OUTPUTS) == lbls.data[i])
+        for (int i = 0; i < train_imgs.size; i++) {
+            one_hot(train_lbls.data[i], y);
+            trainer_train(&trainer, &network, train_imgs.data[i], y, LR);
+            if (argmax(network.output, N_OUTPUTS) == train_lbls.data[i])
                 correct++;
         }
         printf("Epoch %d — Accuracy: %.2f%%\n", epoch + 1,
-               (double)correct / imgs.size * 100.0);
+               (double)correct / train_imgs.size * 100.0);
     }
+
+    // TEST TRAINED MODEL ON UNSEEN DATA
 
     trainer_free(&trainer);
     network_free(&network);
-    free_images(&imgs);
-    free(lbls.data);
+    free_images(&train_imgs);
+    free(train_lbls.data);
     return 0;
 }
